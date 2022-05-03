d9b547613c3e044f22293a20e222eab122d42277
C1325988 Wendel Felipe Nascimento Nogueira <C1325988@interno.bb.com.br>
2022-05-03 12:33:53 -0300

    Webpack: Ajustado configuração do webpack. Realiza build de produção e roda server local

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,20 +1,32 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import directoryConfig from './src/config/directory.config.js';
+import path from 'path';
 
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({ template: 'index.html' });
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({ template: './public/index.html' });
 
 export default {
+  target: 'web',
+  mode: 'development',
   entry: "./src/index.js",
+  resolve: {
+    extensions: ['.js', '.jsx', '.html', '.css'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      lodash: path.resolve('./node_modules/lodash'),
+    },
+  },
   output: {
+    path: directoryConfig.dir.distDir,
     filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js?x$/,
+        test: /\.js|\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
+        loader: 'babel-loader',
+        options: {
+          presets: ['react-app']
         }
       }
     ]
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,33 +1,67 @@
 import Webpack from 'webpack';
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import DuplicatePackageCheckerWebpackPlugin from 'duplicate-package-checker-webpack-plugin';
+import directoryConfig from './src/config/directory.config.js';
 
 var DefinePlugin = new Webpack.DefinePlugin({
   'process.env': {
     NODE_ENV: JSON.stringify('production'),
   },
 });
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({ template: 'index.html' });
-const UglifyPlugin = new Webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }});
-const DedupePlugin = new Webpack.optimize.DedupePlugin();
-const CommonChunksPlugin = new Webpack.optimize.CommonsChunkPlugin({ names: ['vendor', 'manifest'] });
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({ template: './public/index.html' });
+const TerserPluginConfig = new TerserPlugin({
+  parallel: true,
+  terserOptions: {
+    ecma: 6,
+  },
+});
+
+const DuplicatePackageCheckerWebpackPluginConfig = new DuplicatePackageCheckerWebpackPlugin({
+  verbose: true,
+  emitError: true,
+});
+
+const SplitChunksPluginConfig = new Webpack.optimize.SplitChunksPlugin({
+  chunks: 'all',
+  minSize: 30000,
+  maxSize: 0,
+  minChunks: 1,
+  maxAsyncRequests: 5,
+  maxInitialRequests: 3,
+  automaticNameDelimiter: '~',
+  name: true,
+});
 
 export default {
+  target: 'web',
+  mode: 'production',
   entry: {
     vendor: ['react', 'react-dom'],
     app: './src/index.js',
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.html', '.css'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      lodash: path.resolve('./node_modules/lodash'),
+    },
+  },
   output: {
+    path: directoryConfig.dir.distDir,
     filename: '[name].[chunkhash].js',
     chunkFilename: '[chunkhash].bundle.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/,
+        test: /\.js|\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
+        loader: 'babel-loader',
+        options: {
+          presets: ['react-app']
         }
       }
     ]
@@ -35,8 +69,8 @@ export default {
   plugins: [
     DefinePlugin, 
     HTMLWebpackPluginConfig, 
-    UglifyPlugin, 
-    DedupePlugin, 
-    CommonChunksPlugin
-  ],
+    TerserPluginConfig,
+    DuplicatePackageCheckerWebpackPluginConfig, 
+    SplitChunksPluginConfig
+  ]
 }
